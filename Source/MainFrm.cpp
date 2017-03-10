3318c54d0fdd162da2a336e9aee555601ce79d0c
CHEN,TAI-PENG <qwer2003tw@gmail.com>
2017-03-10 10:31:13 +0800

    asdf

--- Source/MainFrm.cpp
+++ Source/MainFrm.cpp
@@ -138,7 +138,7 @@ BOOL CMainFrame::PreCreateWindow(CREATESTRUCT& cs)
 	//	cs.style = WS_OVERLAPPED | WS_CAPTION | FWS_ADDTOTITLE
 	//		| WS_THICKFRAME | WS_SYSMENU | WS_MINIMIZEBOX;
 
-	cs.cx = 640; cs.cy = 480;
+	cs.cx = 1280; cs.cy = 960;
 	cs.style = WS_BORDER | WS_SYSMENU | WS_MINIMIZEBOX | WS_MAXIMIZEBOX;
     cs.x = (::GetSystemMetrics(SM_CXSCREEN) - cs.cx) / 2; 
 	cs.y = (::GetSystemMetrics(SM_CYSCREEN) - cs.cy) / 2; 
