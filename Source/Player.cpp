4777e25d34cebd615647c69652e8de72bacb3532
CHEN,TAI-PENG <qwer2003tw@gmail.com>
2017-03-24 11:54:30 +0800

    3/24 Player image added.

--- Source/Player.cpp
+++ Source/Player.cpp
@@ -15,6 +15,8 @@ namespace game_framework {
         positionNum = 0;
         prePositionNum = 0;
         state = 0;
+        count = 0;
+        direct = 2;
         for (int i = 1; i < 10; i++)                   //產生人物移動路徑座標 0-36
         {
             positionX[i] = positionX[i - 1] + 192;
@@ -38,7 +40,23 @@ namespace game_framework {
     }
     void Player::LoadBitmap()
     {
-        bmp.LoadBitmap("res/ball.bmp", RGB(0, 0, 0));
+        bmp[0][2][0].LoadBitmap("res/doreamon_swap_part1x1.bmp", RGB(0, 0, 0));
+        bmp[0][2][1].LoadBitmap("res/doreamon_swap_part1x2.bmp", RGB(0, 0, 0));
+        bmp[0][2][2].LoadBitmap("res/doreamon_swap_part1x3.bmp", RGB(0, 0, 0));
+        bmp[0][2][3].LoadBitmap("res/doreamon_swap_part1x4.bmp", RGB(0, 0, 0));
+        bmp[0][4][0].LoadBitmap("res/doreamon_swap_part2x1.bmp", RGB(0, 0, 0));
+        bmp[0][4][1].LoadBitmap("res/doreamon_swap_part2x2.bmp", RGB(0, 0, 0));
+        bmp[0][4][2].LoadBitmap("res/doreamon_swap_part2x3.bmp", RGB(0, 0, 0));
+        bmp[0][4][3].LoadBitmap("res/doreamon_swap_part2x4.bmp", RGB(0, 0, 0));
+        bmp[0][6][0].LoadBitmap("res/doreamon_swap_part3x1.bmp", RGB(0, 0, 0));
+        bmp[0][6][1].LoadBitmap("res/doreamon_swap_part3x2.bmp", RGB(0, 0, 0));
+        bmp[0][6][2].LoadBitmap("res/doreamon_swap_part3x3.bmp", RGB(0, 0, 0));
+        bmp[0][6][3].LoadBitmap("res/doreamon_swap_part3x4.bmp", RGB(0, 0, 0));
+        bmp[0][8][0].LoadBitmap("res/doreamon_swap_part4x1.bmp", RGB(0, 0, 0));
+        bmp[0][8][1].LoadBitmap("res/doreamon_swap_part4x2.bmp", RGB(0, 0, 0));
+        bmp[0][8][2].LoadBitmap("res/doreamon_swap_part4x3.bmp", RGB(0, 0, 0));
+        bmp[0][8][3].LoadBitmap("res/doreamon_swap_part4x4.bmp", RGB(0, 0, 0));
+
     }
     void Player::OnMove()
     {       
@@ -49,18 +67,37 @@ namespace game_framework {
                 prePositionNum++;           
                 prePositionNum %= 36;
             }
-            if (dx < positionX[prePositionNum]) dx += 8;
-            else if (dx > positionX[prePositionNum]) dx-=8;
-            if (dy < positionY[prePositionNum]) dy+=8;
-            else if (dy > positionY[prePositionNum]) dy-=8;
+            if (dx < positionX[prePositionNum])
+            {
+                direct = 6;
+                dx += 8;
+            }
+            else if (dx > positionX[prePositionNum])
+            {
+                direct = 4;
+                dx -= 8;
+            }
+            if (dy < positionY[prePositionNum])
+            {
+                direct = 2;
+                dy += 8;
+            }
+            else if (dy > positionY[prePositionNum])
+            {
+                direct = 8;
+                dy -= 8;
+            }
             if (prePositionNum == positionNum && dx == positionX[prePositionNum] && dy == positionY[prePositionNum])
                 state = 0;
         }
     }
     void Player::OnShow(int sx, int sy)
     {
-        bmp.SetTopLeft(dx - bmp.Height() / 2 - sx, dy - bmp.Width() / 2 -sy);
-        bmp.ShowBitmap();
+        if (count >= 20) count = 0;
+        bmp[0][direct][count/5].SetTopLeft(dx - 3*bmp[0][direct][count/5].Height() / 2 - sx, dy -8* bmp[0][direct][count/5].Width() / 2 - sy);
+        bmp[0][direct][count/5].ShowBitmap(5);
+        if (state == 0) direct = 2;
+        if (state != 0)count++;
     }
     int Player::GetMapX()
     {
--- Source/Player.h
+++ Source/Player.h
@@ -11,12 +11,14 @@ namespace game_framework {
         void SetPositionNum(int amount);            // 設定該次累加點數
         void SetState(int ns);                      // 設定狀態
     protected:
-        CMovingBitmap bmp;			                // 人物圖片
+        CMovingBitmap bmp[4][10][5];			    // 人物圖片
         int dx, dy;					                // 人物地圖座標
     private:
         int positionX[36], positionY[36];           // 
         int prePositionNum;                         // 前一次位置點數
         int positionNum;                            // 位置點數
         int state;                                  // 0人物不動狀態 1人物移動狀態
+        int direct;
+        int count;
     };
 }
